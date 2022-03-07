df09c863c9a75329a9428e382b42e40c730a7346
Raul <r4ul96@gmail.com>
2022-03-07 10:20:48 +0000

    Initial commit

---
+++ app/src/main/java/es/travelworld/ejercicio51_toolbar/HomeActivity.java
@@ -0,0 +1,37 @@
+package es.travelworld.ejercicio51_toolbar;
+
+import static es.travelworld.ejercicio51_toolbar.tools.References.KEY_USER;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Objects;
+
+import es.travelworld.ejercicio51_toolbar.databinding.ActivityHomeBinding;
+import es.travelworld.ejercicio51_toolbar.tools.User;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        user = (User)getIntent().getSerializableExtra(KEY_USER);
+
+        setActionBarSettings();
+
+        Log.i("---Datos usuario","Nombre: " + user.getName() + "\nApellidos: " + user.getLastname() + "\nEdad:" + user.getAgeGroup());
+    }
+
+    private void setActionBarSettings() {
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.home_title));
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ac_menu);
+    }
+}
---
+++ app/src/main/java/es/travelworld/ejercicio51_toolbar/tools/References.java
@@ -0,0 +1,5 @@
+package es.travelworld.ejercicio51_toolbar.tools;
+
+public abstract class References {
+    public static final String KEY_USER = "user";
+}
